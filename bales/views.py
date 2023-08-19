444152863c52ea4932558e9e0befd17b3f3f256c
KellyKiiru <infowithkiiru@gmail.com>
2023-08-19 15:15:30 +0300

    define BaleDetail class-based view

--- bales/views.py
+++ bales/views.py
@@ -3,6 +3,7 @@ from rest_framework import parsers
 from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
 
 from bales.models import Bale
@@ -27,20 +28,29 @@ class BaleList(APIView):
 
 
 class BaleDetail(APIView):
-    
-    def get(self,pk, request, format=None):
-        bale = Bale.objects.get(pk)
-        serializer = BaleSerializer(bale, many=True)
+    """
+    Retrieve, update or delete a Bale instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Bale.objects.get(pk=pk)
+        except Bale.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        bale = self.get_object(pk)
+        serializer = BaleSerializer(bale)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        bale = self.get_object(pk)
+        serializer = BaleSerializer(bale, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self,pk, request, format=None):
-        bale = Bale.objects.get(pk)
-        serializer = BaleSerializer(bale, many=True)
-
-    def delete(self,pk, request, format=None):
-        bale = Bale.objects.get(pk)
-        bale.delete()
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request, pk, format=None):
+        bale = self.get_object(pk)
+        Bale.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
