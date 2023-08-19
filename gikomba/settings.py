26856d20ac0a0026b56a012e2c324e802a550cc0
KellyKiiru <infowithkiiru@gmail.com>
2023-08-19 06:17:18 +0300

    add jazzmin and contrib.sites

--- gikomba/settings.py
+++ gikomba/settings.py
@@ -23,12 +23,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'rest_framework',
     'bales'
 ]
