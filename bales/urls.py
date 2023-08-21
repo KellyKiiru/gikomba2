1c4552dc928702b2d7e2b8a2160349c05b1b52ad
KellyKiiru <infowithkiiru@gmail.com>
2023-08-22 00:01:59 +0300

    typo correction

--- bales/urls.py
+++ bales/urls.py
@@ -5,7 +5,7 @@ urlpatterns =[
     path('bales/',BaleList.as_view()),
     path('bales/<int:pk>/',BaleDetail.as_view()),
     path('stores/',StoreList.as_view()),
-    path('stores/<int:pk>/',BaleDetail.as_view()),
+    path('stores/<int:pk>/',StoreDetail.as_view()),
 ]
 
 
