98438b042e12d8daf20a1a59979b79d012ed55cc
KellyKiiru <infowithkiiru@gmail.com>
2023-08-21 23:56:28 +0300

    storeDetail apiview class

--- bales/views.py
+++ bales/views.py
@@ -52,7 +52,7 @@ class BaleDetail(APIView):
 
     def delete(self, request, pk, format=None):
         bale = self.get_object(pk)
-        Bale.delete()
+        bale.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class StoreList(APIView):
@@ -68,3 +68,28 @@ class StoreList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class StoreDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Store.objects.get(pk=pk)
+        except Store.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        store = self.get_object(pk)
+        serializer = StoreSerializer(store)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        store = self.get_object(pk)
+        serializer = StoreSerializer(store, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        store = self.get_object(pk)
+        store.delete()
+        return Response(ststus=status.HTTP_204_NO_CONTENT)
