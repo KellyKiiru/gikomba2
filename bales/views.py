7304eb4c3ac374dc56c9900945daeaa35f92ee85
KellyKiiru <infowithkiiru@gmail.com>
2023-08-22 23:27:57 +0300

    define StoreViewSet

--- bales/views.py
+++ bales/views.py
@@ -13,42 +13,8 @@ class BaleViewSet(viewsets.ReadOnlyModelViewSet):
     
     queryset = Bale.objects.all()
     serializer_class = BaleSerializer
-
-class StoreList(APIView):
-    """Returns the list of stores"""
-    def get(self, request):
-        stores = Store.objects.all()
-        serializer = StoreSerializer(stores, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def post(request):
-        serializer = StoreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class StoreDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Store.objects.get(pk=pk)
-        except Store.DoesNotExist:
-            return Http404
-
-    def get(self, request, pk, format=None):
-        store = self.get_object(pk)
-        serializer = StoreSerializer(store)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        store = self.get_object(pk)
-        serializer = StoreSerializer(store, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        store = self.get_object(pk)
-        store.delete()
-        return Response(ststus=status.HTTP_204_NO_CONTENT)
+class StoreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
