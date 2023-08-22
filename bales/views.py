26de256452efe02c4f9f3d1ca5a6b0516133a728
KellyKiiru <infowithkiiru@gmail.com>
2023-08-22 23:26:51 +0300

    delete redundant code

--- bales/views.py
+++ bales/views.py
@@ -14,35 +14,6 @@ class BaleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Bale.objects.all()
     serializer_class = BaleSerializer
 
-
-class BaleDetail(APIView):
-    """
-    Retrieve, update or delete a Bale instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Bale.objects.get(pk=pk)
-        except Bale.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        bale = self.get_object(pk)
-        serializer = BaleSerializer(bale)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        bale = self.get_object(pk)
-        serializer = BaleSerializer(bale, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        bale = self.get_object(pk)
-        bale.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 class StoreList(APIView):
     """Returns the list of stores"""
     def get(self, request):
