54c0ea858930e49fbcff5462b5e375bda19e3545
KellyKiiru <infowithkiiru@gmail.com>
2023-08-22 23:30:46 +0300

    bale_list url binding to viewset

--- bales/urls.py
+++ bales/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
 
+bale_list = BaleViewSet.as_view({
+    'get':'list',
+    'post':'create'
+})
+
 urlpatterns =[
-    path('bales/',BaleList.as_view()),
+    path('bales/',bale_list),
     path('bales/<int:pk>/',BaleDetail.as_view()),
     path('stores/',StoreList.as_view()),
     path('stores/<int:pk>/',StoreDetail.as_view()),
