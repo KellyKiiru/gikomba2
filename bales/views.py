dd80a3461a05227434e5752081916b1875a5f424
KellyKiiru <infowithkiiru@gmail.com>
2023-08-22 23:26:15 +0300

    define BaleViewSet class

--- bales/views.py
+++ bales/views.py
@@ -1,5 +1,5 @@
 
-from rest_framework import parsers
+from rest_framework import viewsets
 from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,22 +9,10 @@ from django.http import Http404
 from bales.models import *
 from bales.serializers import *
 
-class BaleList(APIView):
-    """
-    List all bales or create a new bale.
-    """
-    def get(self, request, format=None):
-        bales = Bale.objects.all()
-        serializer = BaleSerializer(bales, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = BaleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+class BaleViewSet(viewsets.ReadOnlyModelViewSet):
+    
+    queryset = Bale.objects.all()
+    serializer_class = BaleSerializer
 
 
 class BaleDetail(APIView):
